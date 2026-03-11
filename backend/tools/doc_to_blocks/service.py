d771eb4e31393d662063406b8be936ddf4e7321c
timka6023 <okuttimur@gmail.com>
2026-03-11 17:49:36 +1000

    add script

--- backend/tools/doc_to_blocks/service.py
+++ backend/tools/doc_to_blocks/service.py
@@ -1,22 +1,51 @@
 from docx import Document
 def convert_doc_to_wp_tags(file):
-    """
-    Данная функция просто возвращает количество символов в документе докх
-    """
-    try:
-        doc = Document(file)
+    from docx import Document
 
-        full_text = []
-        for para in doc.paragraphs:
-            full_text.append(para.text)
+    document = Document('input.docx')
+    table_text = []  # список с таблицей
 
-        content = "\n".join(full_text)
+    for table in document.tables:  # заполнение таблицы
+        for row in table.rows:
+            text = [cell.text for cell in row.cells]
+            table_text.append(text)
 
-        # Считаем символы
-        total_chars_with_spaces = len(content)
-        total_chars_no_spaces = len(content.replace(" ", "").replace("\n", "").replace("\r", ""))
+    atistation = table_text[-4:-3]
+    table_text = table_text[2:-5]  # убираю лишнее
 
+    with open('output.txt', 'wb') as output:
+        with open('sample_without_raspisanie_zanatiy.html', 'rb') as sample:
+            output.write(sample.read())
 
-        return f" количество символов с пробелами - {total_chars_with_spaces}, Количество символов без пробелов - {total_chars_no_spaces}"
-    except Exception as e:
-        return f"Ошибка при чтении документа: {str(e)}"
+    with open('output.txt', 'ab') as output:
+        sample = open('sample.html', 'rb')
+        date_sample_start = sample.read(638)  # переменая с тегами перед текстом с датой
+        date_sample_end = sample.read(66)  # переменая с тегами после текста с датой
+        main_text_tegs = sample.read(235)
+
+        for row_index in range(len(table_text)):
+            time = table_text[row_index][0]
+            lecturer = table_text[row_index][3]
+            lesson = table_text[row_index][1]
+
+            if lesson[:6] == 'Модуль':
+                continue
+            elif time == lesson:
+                date = table_text[row_index][0]
+                print(date[:5])
+                output.write(date_sample_start + bytes(date, 'utf-8') + date_sample_end)
+                output.write(main_text_tegs)
+
+            else:
+                output.write(bytes(time.replace('.', ':') + ', ' + lecturer.split(',')[0], "utf-8"))
+                output.write(bytes("<br></strong>" + lesson, "utf-8"))
+
+                if row_index == len(table_text) - 1 or table_text[row_index + 1][0] == table_text[row_index + 1][1] and \
+                        table_text[row_index + 1][0][:6] != 'Модуль':
+                    output.write(bytes("</p>\n<!-- /wp:paragraph -->\n\n", "utf-8"))
+
+                else:
+                    output.write(bytes("<br><strong>", "utf-8"))
+
+        sample.close()
+        return output
