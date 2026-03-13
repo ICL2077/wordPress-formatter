733297a50119970f8155f2c708d883d9bf7559a1
Amir Rachimov <banan_2007@inbox.ru>
2026-03-13 13:58:31 +1000

    text commit: хочу попробовать сделать изменения в уже задеплоенном проекте

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
     return (
-        <header className={cn('bg-blue-500 w-full', className)}>
+        <header className={cn('bg-black w-full', className)}>
             <Container className="py-3 flex justify-between flex-col gap-1 text-white">
                 <h1>WordPress formatter</h1>
             </Container>
