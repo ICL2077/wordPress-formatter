8cbb4766b7eba0d34cfb65d87b4d5a3f98d2865d
Gavriilbolt <barogavrooon@gmail.com>
2026-03-18 14:06:00 +1000

    подготовился к слиянию с master

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/frontend/components/shared/header";
+
+const nunitoFont = Nunito({
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "WordPress formatter",
+  description: "Сайт для форматирования word-файлов в html",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${nunitoFont.variable} ${nunitoFont.variable} antialiased`}
+      >
+        <main className="min-h-screen">
+          <Header />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,23 @@
 
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { Container } from "@/components/shared/container";
+import { Container } from "@/frontend/components/shared/container";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL не задан");
+}
+console.log("API:", API_URL);
+
+function getApiUrl() {
+  const url = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!url) {
+    throw new Error("NEXT_PUBLIC_API_URL не задан");
+  }
+
+  return url;
+}
 
 export default function HomePage() {
   const [htmlString, setHtmlString] = useState<string>("");
@@ -33,7 +49,7 @@ export default function HomePage() {
         formData.append("file", file);
 
         const response = await fetch(
-          "http://localhost:5001/api/tools/doc-to-blocks/",
+          `${getApiUrl()}/api/tools/doc-to-blocks/`,
           {
             method: "POST",
             body: formData,
@@ -52,9 +68,12 @@ export default function HomePage() {
         }
 
         setHtmlString(data.html);
-      } catch (err: any) {
-        console.error(err);
-        setError(err.message || "Ошибка загрузки");
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Неизвестная ошибка");
+        }
       } finally {
         setLoading(false);
       }
