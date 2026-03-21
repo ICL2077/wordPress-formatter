e9a064a252d01a66adbf2b1a5611b04102a0159c
Amir Rachimov <banan_2007@inbox.ru>
2026-03-21 13:19:36 +1000

    исправил визуальный баг, изменил шаблон-фай, улучшил адаптивность

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -61,7 +61,7 @@ export default function HomePage() {
     };
 
     return (
-        <Container className="flex h-[90vh] gap-5 flex-row py-5 overflow-hidden max-[1030px]:flex-col max-[1030px]:px-3">
+        <Container className="flex h-[90vh] gap-5 flex-row py-5  max-[1030px]:flex-col max-[1030px]:px-3">
             {/* DROPZONE */}
             <div
                 {...getRootProps()}
@@ -78,14 +78,20 @@ export default function HomePage() {
 
             {/* RESULT */}
             <div className="w-full h-full flex flex-col rounded bg-gray-100 p-5">
-                <div className="flex justify-between items-center mb-3">
-                    <h4 className="font-bold">
-                        {Boolean(htmlString)
-                            ? `Вот ваши спарсенные данные!`
-                            : `Тут будут ваши спарсенные данные!`}
-                    </h4>
+                <div className="flex justify-between items-center mb-3 max-[1030px]:flex-col max-[1030px]:gap-3">
+                    <div className="flex-col">
+                        <h4 className="font-bold">
+                            {Boolean(htmlString)
+                                ? `Вот ваши спарсенные данные!`
+                                : `Тут будут ваши спарсенные данные!`}
+                        </h4>
 
-                    <div className="flex flex-row gap-3 items-center">
+                        {!htmlString && !error && !isLoading && !postFileError && (
+                            <p className="text-gray-500">Скиньте уже что-нибудь!</p>
+                        )}
+                    </div>
+
+                    <div className="flex flex-row gap-3 items-center max-[1030px]:flex-col">
                         <div className="relative flex items-center justify-center">
                             <OutsideClickHandler onOutsideClick={() => setIsOpened(false)}>
                                 <button
@@ -97,8 +103,8 @@ export default function HomePage() {
 
                             <div
                                 className={cn(
-                                    'flex flex-col gap-3 absolute invisible transition-all duration-200 translate-y-3 opacity-0 top-8 h-fit w-75 border border-black bg-white z-30 rounded-md px-1 py-2 overflow-hidden',
-                                    isOpened && 'visible opacity-100 translate-y-0',
+                                    'flex flex-col gap-3 absolute transition-all duration-200 translate-y-3 opacity-0 top-8 h-fit w-75 border border-black bg-white z-30 rounded-md px-1 py-2 overflow-hidden',
+                                    isOpened && 'opacity-100 translate-y-0',
                                 )}>
                                 <p>
                                     Вы можете скачать файл-шаблон для того чтобы увидеть как
@@ -124,10 +130,6 @@ export default function HomePage() {
                 </div>
 
                 {error && <p className="text-red-500 mb-3">{error}</p>}
-
-                {!htmlString && !error && !isLoading && !postFileError && (
-                    <p className="text-gray-500">Скиньте уже что-нибудь!</p>
-                )}
 
                 {isLoading ? (
                     <h1 className="m-auto">Загрузка...</h1>
